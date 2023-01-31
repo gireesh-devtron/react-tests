32ac609fd3c9590ae8d86943388c785e6e6be1dd
Gireesh Naidu <gireesh@devtron.ai>
2023-01-31 09:05:38 +0530

    error screen

--- src/components/PageNotFound.tsx
+++ src/components/PageNotFound.tsx
@@ -3,8 +3,8 @@ const PageNotFound = () => {
     <>
       <section className="col-12">
         <h1 className="display-1 text-center text-info mt-5 pt-5">
-          404 Page Not Found
-          <i className="ms-1 fa fa-frown-o" aria-hidden="true"></i>
+          404 Error Page Not Found
+          <i className="ms-2 fa fa-frown-o" aria-hidden="true"></i>
         </h1>
       </section>
     </>
